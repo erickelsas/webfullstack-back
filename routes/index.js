d8b8fce3ac5b6a16e48631fd2742243e7b4d4818
Erick Freitas <erickelsas@alunos.utfpr.edu.br>
2025-02-12 21:24:27 -0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const userRoutes = require('./userRoutes');
 const bookRoutes = require('./bookRoutes');
 const authorRoutes = require('./authorRoutes');
 const installRoutes = require('./installRoutes');
-const multerRoutes = require('./multerRoutes');
+const multerRoutes = require('./multerRoute');
 
 module.exports = {
     userRoutes,
